be2a7690c6ea0ef686399f5e1612c1c319484dfb
JP <jpellerin@leapfrogonline.com>
2012-04-12 16:02:55 -0400

    Update bootstrap script for new submodules

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,8 @@
 #
 # NOTE: requires virtualenvwrapper
 #
+git submodule init
+git submodule update --recursive
 mkvirtualenv nose2
 pip install -r requirements.txt
 python setup.py develop
-pushd docs
-git clone http://github.com/mitsuhiko/flask-sphinx-themes.git _themes
-popd
